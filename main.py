7ec1ddd8c21da46f3c0507815eaa4ab346c26d0d
muxammad <ibrohimovmuhammad2020@gmail.com>
2024-07-20 17:06:17 +0200

    full project

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from transliterate import to_cyrillic, to_latin
+import telebot
 
-sentence = input("Matn Kiriting: ")
-if sentence.isascii():
-    print(to_cyrillic(sentence))
-else:
-    print(to_latin(sentence))
+Token = '6339346788:AAGEeM5PYnasZDZotGRnTcLkhMp3XQDIIxI'
+bot = telebot.TeleBot(Token, parse_mode=None)
+
+
+@bot.message_handler(commands=['start'])
+def greeting(message):
+    bot.reply_to(message, "Hey, glad to see you here ;)")
+
+
+@bot.message_handler(func=lambda message: True)
+def echo(message):
+    msg = message.text
+    result = lambda msg: to_cyrillic(msg) if msg.isascii() else to_latin(msg)
+    bot.reply_to(message, result(msg))
+
+
+bot.polling()
